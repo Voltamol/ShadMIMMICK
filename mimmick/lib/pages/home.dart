6c1b2e78487ae26d23e0720eadc1afa2c14f2332
Voltamol <119132016+Voltamol@users.noreply.github.com>
2024-10-23 23:47:18 +0200

    Update home.dart

--- mimmick/lib/pages/home.dart
+++ mimmick/lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:mimmick/components/cards/product_card_with_cta.dart';
+import 'package:mimmick/components/user_type/user_type.dart';
 import 'package:shadcn_flutter/shadcn_flutter.dart';
 
 class Home extends StatelessWidget {
@@ -7,15 +8,23 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      child: Center(
-        child: SizedBox(
+      child: 
+      Column(
+        crossAxisAlignment:CrossAxisAlignment.center,
+        children:[
+          UserType(),
+          SizedBox(height:20),
+           SizedBox(
             width: 320,
             child: ProductCardWithCta(
               title: "NIKE Sweat-absorbing running T-shirt",
               content: "80.10",
               liked: true,
-            )),
-      ),
+            )
+           )
+        ]
+      )
+    
     );
   }
 }
