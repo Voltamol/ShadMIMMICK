153776c9a42c9dce7a943040249da68574e6954c
Voltamol <119132016+Voltamol@users.noreply.github.com>
2024-10-24 12:31:56 +0200

    Update swiper-card.dart

--- mimmick/lib/components/cards/swiper-card.dart
+++ mimmick/lib/components/cards/swiper-card.dart
@@ -16,13 +16,9 @@ class _ExampleState extends State<Example> {
     super.dispose();
   }
 
-  final List<Container> cards = [
-    Container(
-      alignment: Alignment.center,
-      child: const Text('1'),
-      decoration: BoxDecoration(
-          color: Colors.blue, borderRadius: BorderRadius.circular(8)),
-    ),
+  final List<Widget> cards = [
+    const ProfileCard(
+        title: "voltamol", age: 24, color: Colors.green, location: "Harare"),
     Container(
       alignment: Alignment.center,
       child: const Text('2'),
@@ -60,29 +56,20 @@ class _ExampleState extends State<Example> {
             children: [
               FloatingActionButton(
                   onPressed: () => controller.swipe(CardSwiperDirection.left),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: AnimatedEmoji(
-                      AnimatedEmojis.fromEmojiString('😒')!,
-                      size: 100,
-                    ),
+                  child: const Icon(
+                    Icons.cancel_outlined,
+                    color: Colors.red,
                   )),
               const SizedBox(
                 width: 10,
               ),
-              BookMarkButton(),
+              const BookMarkButton(),
               const SizedBox(
                 width: 10,
               ),
               FloatingActionButton(
                   onPressed: () => controller.swipe(CardSwiperDirection.right),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: AnimatedEmoji(
-                      AnimatedEmojis.fromEmojiString('👍')!,
-                      size: 100,
-                    ),
-                  )),
+                  child: const Icon(Icons.favorite_outline)),
             ], // Added this closing bracket
           ), // Added this closing bracket
         ), // Added this closing bracket
@@ -113,6 +100,73 @@ class _ExampleState extends State<Example> {
   }
 }
 
+class ProfileCard extends StatelessWidget {
+  final String title;
+  final String location;
+  final int age;
+  final color;
+  const ProfileCard({
+    super.key,
+    required this.title,
+    required this.location,
+    required this.age,
+    required this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      decoration:
+          BoxDecoration(color: color, borderRadius: BorderRadius.circular(8)),
+      child: Stack(
+        children: [
+          //image here
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+                child: Container(
+                  width: 300,
+                  height: 80,
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8)),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("$title, ${age.toString()}",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w800, fontSize: 24)),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Icon(
+                            Icons.location_pin,
+                            color: color,
+                          ),
+                          Text(
+                            location,
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
 class BookMarkButton extends StatefulWidget {
   const BookMarkButton({
     super.key,
@@ -124,11 +178,16 @@ class BookMarkButton extends StatefulWidget {
 
 class _BookMarkButtonState extends State<BookMarkButton> {
   bool clicked = false;
+  void toggleIcon() {
+    setState(() {
+      clicked = !clicked;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
-      onPressed: () {},
+      onPressed: toggleIcon,
       child: Icon(
         clicked ? Icons.bookmark : Icons.bookmark_outline,
       ),
